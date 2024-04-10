9307de6bfc3e7585c00dfe92d62b42e68d466e78
PeterYaoNYU <yy4108@nyu.edu>
2024-04-09 21:15:10 -0400

    finally finish all in ps6

--- ps6/mlish_compile.ml
+++ ps6/mlish_compile.ml
@@ -19,6 +19,8 @@ let rec compile_prim (p:ML.prim) : S.primop =
   | ML.Cons -> S.Cons
   | _ -> raise UnavailType
 
+(* key idea to represent Nil: use the first element of the list to store the length *)
+
 
 let rec compile_exp_aux ((e,pos):ML.exp) : S.exp =
   (* print_endline (ML.exp2string (e, pos));  *)
@@ -31,17 +33,20 @@ let rec compile_exp_aux ((e,pos):ML.exp) : S.exp =
       | ML.Int i, _ -> S.Int i
       | ML.Bool true, _ -> S.Int 1
       | ML.Bool false, _ -> S.Int 0
-      | ML.IsNil, [(e1, pos)]-> S.If (S.PrimApp (S.Eq, [compile_exp_aux (e1, pos); S.Var "()"]), S.Int 1, S.Int 0)
+      | ML.IsNil, [(e1, pos)]-> S.If (S.PrimApp (S.Eq, [S.PrimApp(S.Fst, [compile_exp_aux (e1, pos)])  ; S.Int 0]),
+                                       S.Int 1, S.Int 0)
       | ML.Cons, [e1; e2] ->
-        (* print_endline "Compiling Cons...";   *)
-        (
-          match e2 with
-          | ML.PrimApp (ML.Nil, []), 0 -> compile_exp_aux e1
-          | _ -> S.PrimApp (S.Cons, [compile_exp_aux e1; compile_exp_aux e2])
-        )
-      | ML.Hd, [e] -> S.PrimApp (S.Fst, [compile_exp_aux e])
-      | ML.Tl, [e] -> S.PrimApp (S.Snd, [compile_exp_aux e])  
-      | ML.Nil, [] ->  S.Var "()"
+        S.PrimApp (S.Cons, [S.PrimApp(S.Plus, [S.Int 1; S.PrimApp(S.Fst, [compile_exp_aux e2])]);
+                             S.PrimApp(S.Cons, [compile_exp_aux e1 ; 
+                             S.PrimApp(S.Snd, [compile_exp_aux e2])])])
+      | ML.Hd, [e] -> S.If (S.PrimApp(S.Eq, [S.PrimApp(S.Fst, [compile_exp_aux e]); S.Int 0]), 
+                            (compile_exp_aux e), 
+                            S.PrimApp(S.Fst, [S.PrimApp(S.Snd, [compile_exp_aux e])]))
+      | ML.Tl, [e] -> S.If (S.PrimApp(S.Eq, [S.PrimApp(S.Fst, [compile_exp_aux e]); S.Int 0]), 
+                            S.PrimApp (S.Cons, [S.Int 0; S.Int 0]), 
+                            (S.PrimApp(S.Cons, [S.PrimApp(S.Minus, [S.PrimApp(S.Fst, [compile_exp_aux e]); S.Int 1]);
+                                          S.PrimApp(S.Snd, [S.PrimApp(S.Snd, List.map compile_exp_aux es)])])))
+      | ML.Nil, [] ->  S.PrimApp (S.Cons, [S.Int 0; S.Int 0])
       | _ -> S.PrimApp (compile_prim p, List.map compile_exp_aux es)
     )
   | ML.Fn (x, e) ->  S.Lambda (x, compile_exp_aux e)
@@ -50,5 +55,5 @@ let rec compile_exp_aux ((e,pos):ML.exp) : S.exp =
   | ML.Let (x, e1, e2) -> S.sLet x (compile_exp_aux e1) (compile_exp_aux e2)
 
 let compile_exp ((e, pos): ML.exp) =
-  S.sLet "()" (S.Int 0) (compile_exp_aux (e, pos))
+  (compile_exp_aux (e, pos))
   
